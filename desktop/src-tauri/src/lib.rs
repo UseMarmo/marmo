53957632fa8d9cfb885324f4c13a9b0859debd61
Marmo <marmoteam@outlook.com>
2026-06-19 14:32:28 +0100

    fix: set WEBKIT_DISABLE_DMABUF_RENDERER before builder to prevent EGL crash on Mesa 26.x

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 use tauri::Manager;
 
 pub fn run() {
+    #[cfg(target_os = "linux")]
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
